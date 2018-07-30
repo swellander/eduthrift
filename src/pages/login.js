a86d80cf9860cd1c1d2bdbbfce7c2f1baf2249a2
Jon Chin <jon@sharebyteaching.org>
2018-07-30 07:53:40 -0400

    updated for auth implementation

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,18 +1,115 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { compose } from 'redux'
+import { connect } from 'react-redux'
+import { firebaseConnect, isLoaded, isEmpty } from 'react-redux-firebase'
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import FormControl from '@material-ui/core/FormControl';
 
 class LoginPage extends React.Component{
+    constructor(props){
+	super(props);
+	this.state = {
+	    email: '',
+	    password: ''
+	};
+    }
+    
+    handleChange(event, field){
+	this.setState({
+	    [field]: event.target.value
+	});
+    }
+
+    handleSubmit(event){
+	event.preventDefault();
+	this.props.firebase.login(this.state)
+	    .then((response) => {
+		console.log(response);
+		console.log(this.props);
+	    })
+	    .catch((error) => {
+		switch(error.code){
+		    case 'auth/user-not-found':
+			// do something
+			break;
+		    case 'auth/wrong-password':
+		    case 'auth/invalid-email':
+			// do something
+			break;
+		    case 'auth/network-request-failed':
+			// do something
+			break;
+		    default:
+			// default error
+		}
+	    });
+    }
+
+    logout(){
+	this.props.firebase.logout();
+    }
+    
     render(){
+	let payload;
+	if(!this.props.auth.isLoaded){
+	    // auth is not warmed up
+	    payload = null;
+	}
+	if(this.props.auth.isLoaded && this.props.auth.isEmpty){
+	    // auth is ready
+	    // but user is not logged in
+	    payload = <form onSubmit={(event) => {this.handleSubmit(event);}}>
+		<FormControl fullWidth>
+		    <TextField
+			label="Email"
+			value={this.state.email}
+			onChange={(event) => {this.handleChange(event, 'email');}}
+			margin="normal"
+		    />
+		</FormControl>
+		<FormControl fullWidth>
+		    <TextField
+			label="Password"
+			type="password"
+			value={this.state.password}
+			onChange={(event) => {this.handleChange(event, 'password');}}
+			margin="normal"
+		    />
+		</FormControl>
+		<Button type="submit"
+			variant="contained"
+			color="primary">
+		    Login
+		</Button>
+	    </form>;
+	}
+	if(this.props.auth.isLoaded && !this.props.auth.isEmpty){
+	    // auth is warmed up
+	    // and user is not logged in
+	    payload = <div>
+		<div>
+		    Welcome {this.props.auth.email}
+		</div>
+		<div>
+		    <Button variant="contained"
+			    color="secondary"
+			    onClick={() => {this.props.firebase.logout();}}>
+			Logout
+		    </Button>
+		    
+		</div>
+	    </div>;
+	}
 	return(
 	    <div>
-		<h1>
-		    Login Page
-		</h1>
-		<Link to="/">Home</Link><br />
-		<Link to="/signup">Signup</Link>
+		{payload}
 	    </div>
 	)
     }
 };
 
-export default LoginPage
+export default compose(
+    firebaseConnect(),
+    connect(({firebase: {auth}}) => ({auth}))
+)(LoginPage);
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,18 +1,109 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { compose } from 'redux'
+import { connect } from 'react-redux'
+import { firebaseConnect, isLoaded, isEmpty } from 'react-redux-firebase'
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import FormControl from '@material-ui/core/FormControl';
 
 class SignupPage extends React.Component{
+    constructor(props){
+	super(props);
+	this.state = {
+	    email: '',
+	    password: ''
+	};
+    }
+    
+    handleChange(event, field){
+	this.setState({
+	    [field]: event.target.value
+	});
+    }
+
+    handleSubmit(event){
+	event.preventDefault();
+	this.props.firebase.createUser(this.state)
+	    .then((response) => {
+		// do something
+	    })
+	    .catch((error) => {
+		switch(error.code){
+		    case 'auth/email-already-in-use':
+			// do something
+			break;
+		    case 'auth/invalid-email':
+			break;
+		    case 'auth/operation-not-allowed':
+			// do something
+			break;
+		    case 'auth/weak-password':
+			// do something
+			break;
+		    default:
+			// default error
+		}
+	    });
+    }
+    
     render(){
+	let payload;
+	if(!this.props.auth.isLoaded){
+	    // auth is not warmed up
+	    payload = null;
+	}
+	if(this.props.auth.isLoaded && this.props.auth.isEmpty){
+	    // auth is ready
+	    // but user is not logged in
+	    payload = <form onSubmit={(event) => {this.handleSubmit(event);}}>
+		<FormControl fullWidth>
+		    <TextField
+			label="Email"
+			value={this.state.email}
+			onChange={(event) => {this.handleChange(event, 'email');}}
+			margin="normal"
+		    />
+		</FormControl>
+		<FormControl fullWidth>
+		    <TextField
+			label="Password"
+			type="password"
+			value={this.state.password}
+			onChange={(event) => {this.handleChange(event, 'password');}}
+			margin="normal"
+		    />
+		</FormControl>
+		<Button type="submit"
+			variant="contained"
+			color="primary">Signup</Button>
+	    </form>;
+	}
+	if(this.props.auth.isLoaded && !this.props.auth.isEmpty){
+	    // auth is warmed up
+	    // and user is not logged in
+	    payload = <div>
+		<div>
+		    Welcome {this.props.auth.email}
+		</div>
+		<div>
+		    <Button variant="contained"
+			    color="secondary"
+			    onClick={() => {this.props.firebase.logout();}}>
+			Logout
+		    </Button>
+		    
+		</div>
+	    </div>;
+	}
 	return(
 	    <div>
-		<h1>
-		    Signup Page
-		</h1>
-		<Link to="/">Home</Link><br />
-		<Link to="/login">Login</Link>
+		{payload}
 	    </div>
 	)
     }
 };
 
-export default SignupPage
+export default compose(
+    firebaseConnect(),
+    connect(({firebase: {auth}}) => ({auth}))
+)(SignupPage);
