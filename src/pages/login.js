4ccc680fd201c699c6888ac4c5a716425cb8e1ae
Sam Wellander <samwellander@gmail.com>
2018-10-13 08:24:30 -0400

    email

--- src/pages/login.js
+++ src/pages/login.js
@@ -11,119 +11,120 @@ import PropTypes from 'prop-types';
 import { Redirect } from 'react-router-dom'
 import Grid from '@material-ui/core/Grid';
 
-class LoginPage extends React.Component{
+class LoginPage extends React.Component {
 
-  componentDidMount() {
-    window.scrollTo(0, 0)
-  }
+	componentDidMount() {
+		window.scrollTo(0, 0)
+	}
 
-    constructor(props){
-	super(props);
-	this.state = {
-	    email: '',
-	    password: '',
-      firstname: '',
-      lastname: ''
-	};
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			email: '',
+			password: '',
+			firstname: '',
+			lastname: ''
+		};
+	}
 
-    handleChange(event, field){
-	this.setState({
-	    [field]: event.target.value
-	});
-    }
+	handleChange(event, field) {
+		this.setState({
+			[field]: event.target.value
+		});
+	}
 
-    handleSubmit(event){
-	event.preventDefault();
-	this.props.firebase.login(this.state)
-	    .then((response) => {
-        this.props.history.push('/profile')
-		// do something
-	    })
-	    .catch((error) => {
-		switch(error.code){
-		    case 'auth/user-not-found':
-			// do something
-			break;
-		    case 'auth/wrong-password':
-		    case 'auth/invalid-email':
-			// do something
-			break;
-		    case 'auth/network-request-failed':
-			// do something
-			break;
-		    default:
-			// default error
+	handleSubmit(event) {
+		event.preventDefault();
+		this.props.firebase.login(this.state)
+			.then((response) => {
+				this.props.history.push('/profile')
+				// do something
+			})
+			.catch((error) => {
+				switch (error.code) {
+					case 'auth/user-not-found':
+						// do something
+						break;
+					case 'auth/wrong-password':
+					case 'auth/invalid-email':
+						// do something
+						break;
+					case 'auth/network-request-failed':
+						// do something
+						break;
+					default:
+					// default error
+				}
+			});
+	}
+
+	logout() {
+		this.props.firebase.logout();
+	}
+
+	render() {
+		let payload;
+		if (!this.props.auth.isLoaded) {
+			// auth is not warmed up
+			payload = null;
 		}
-	    });
-    }
-
-    logout(){
-	this.props.firebase.logout();
-    }
-
-    render(){
-	let payload;
-	if(!this.props.auth.isLoaded){
-	    // auth is not warmed up
-	    payload = null;
-	}
-	if(this.props.auth.isLoaded && this.props.auth.isEmpty){
-	    // auth is ready
-	    // but user is not logged in
-	    payload =
-      <form onSubmit={(event) => {this.handleSubmit(event);}}>
-		<FormControl fullWidth>
-		    <TextField
-			label="Email"
-			value={this.state.email}
-			onChange={(event) => {this.handleChange(event, 'email');}}
-			margin="normal"
-		    />
-		</FormControl>
-		<FormControl fullWidth>
-		    <TextField
-			label="Password"
-			type="password"
-			value={this.state.password}
-			onChange={(event) => {this.handleChange(event, 'password');}}
-			margin="normal"
-		    />
-		</FormControl>
-		<Button type="submit"
-			variant="contained"
-			color="primary">
-		    Login
+		if (this.props.auth.isLoaded && this.props.auth.isEmpty) {
+			// auth is ready
+			// but user is not logged in
+			payload =
+				<form onSubmit={(event) => { this.handleSubmit(event); }}>
+					<FormControl fullWidth>
+						<TextField
+							label="Email"
+							value={this.state.email}
+							onChange={(event) => { this.handleChange(event, 'email'); }}
+							margin="normal"
+						/>
+					</FormControl>
+					<FormControl fullWidth>
+						<TextField
+							label="Password"
+							type="password"
+							value={this.state.password}
+							onChange={(event) => { this.handleChange(event, 'password'); }}
+							margin="normal"
+						/>
+					</FormControl>
+					<Button type="submit"
+						variant="contained"
+						color="primary">
+						Login
 		</Button>
-	    </form>;
-	}
-	if(this.props.auth.isLoaded && !this.props.auth.isEmpty){
-	    // auth is warmed up
-	    // and user is not logged in
-	    payload = <div>
-		<div>
-		    Welcome {this.props.auth.email}
-		</div>
-		<div>
-		    <Button variant="contained"
-			    color="secondary"
-			    onClick={() => {this.props.firebase.logout();}}>
-			Logout
+				</form>;
+		}
+		if (this.props.auth.isLoaded && !this.props.auth.isEmpty) {
+			console.log(this.props.auth);
+			// auth is warmed up
+			// and user is not logged in
+			payload = <div>
+				<div>
+					Welcome {this.props.auth.email}
+				</div>
+				<div>
+					<Button variant="contained"
+						color="secondary"
+						onClick={() => { this.props.firebase.logout(); }}>
+						Logout
 		    </Button>
 
-		</div>
-	    </div>;
+				</div>
+			</div>;
+		}
+		return (
+			<div>
+				{payload}
+			</div>
+		)
 	}
-	return(
-	    <div>
-		{payload}
-	    </div>
-	)
-    }
 };
 
 export default compose(
-    firebaseConnect(),
-    connect(({firebase: {auth}}) => ({auth})),
-      connect(({ firebase: { profile } }) => ({ profile }))
+	firebaseConnect(),
+	connect(({ firebase: { auth } }) => ({ auth })),
+	connect(({ firebase: { profile } }) => ({ profile }))
 )(LoginPage);
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,21 @@
+const key = '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';
+const sgMail = require('@sendgrid/mail');
+
+export const sendEmail = (toAddress, matchedUser) => {
+  console.log('sending')
+  sgMail.setApiKey(key);
+  const msg = {
+    to: toAddress,
+    from: 'samwellander@gmail.com',
+    subject: 'You have got a match!',
+    text: 'and easy to do anywhere, even with Node.js',
+    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+  };
+  sgMail.send(msg);
+  console.log('sent')
+}
+
+
 export const isMatch = (bookObj, bookArray) => {
   const title = bookObj.title.toLowerCase();
   const author = bookObj.author.toLowerCase();
